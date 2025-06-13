1371cbf74563ea8571e2d184b6213d11698ec1c7
Buddha <aleisandrj@gmail.com>
2025-06-13 16:54:47 +0300

    Task 2 is readi

---
+++ CleverensSoftTask2/CleverensSoftTask2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace CleverensSoftTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5 читателей 
+            for(int i = 0; i < 5; i++)
+            {
+                new Thread(ReadServer).Start();
+            }
+            // 2 писателя
+            for (int i = 0; i < 2; i++)
+            {
+                new Thread(WriteServer).Start();
+            }
+
+            static void ReadServer()
+            {
+                while (true)
+                {
+                    int c = Server.GetCount();
+                    Console.WriteLine($"[Reader] Count = {c}");
+                    Thread.Sleep(600);
+                }
+            }
+            static void WriteServer()
+            {
+                while (true)
+                {
+                    Server.AddToCount(5);
+                    Console.WriteLine("[Writer] +1 to count");
+                    Thread.Sleep(2000);
+                }
+            }
+        }
+    }
+}
---
+++ CleverensSoftTask2/CleverensSoftTask2/Server.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleverensSoftTask2
+{
+    public static class Server
+    {
+        private static int count = 0;
+        private static ReaderWriterLockSlim lockt = new ReaderWriterLockSlim();
+
+        public static int GetCount()
+        {
+            lockt.EnterReadLock();
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                lockt.ExitReadLock();
+            }
+        }
+        public static void AddToCount(int value)
+        {
+            lockt.EnterWriteLock();
+            try
+            {
+                count =+ value;
+            }
+            finally
+            {
+                lockt.ExitWriteLock();
+            }
+        }
+
+    }
+}
